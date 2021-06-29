f13e87508cb0c087366faf258301622a0d75abf9
Maria Camila Fetecua <maria.fetecua@mail.escuelaing.edu.co>
2021-06-29 15:33:13 -0500

    Arreglando para desplegar en Heroku

--- src/main/java/edu/escuelaing/arsw/wbinteractiverealtimeline/WBAppController.java
+++ src/main/java/edu/escuelaing/arsw/wbinteractiverealtimeline/WBAppController.java
@@ -3,11 +3,23 @@ package edu.escuelaing.arsw.wbinteractiverealtimeline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class WBAppController {
 
     public static void main(String[] args) {
-        SpringApplication.run(WBAppController.class, args);
+
+        SpringApplication app = new SpringApplication(WBAppController.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", getPort()));
+        app.run(args);
+    }
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 
 }
--- src/main/resources/static/js/WBComponents.jsx
+++ src/main/resources/static/js/WBComponents.jsx
@@ -62,7 +62,10 @@ render()
 }
 // Retorna la url del servicio. Es una función de configuración.
 function BBServiceURL() {
-    return 'ws://localhost:8080/bbService';
+    var host = window.location.host;
+    var url = 'wss://' + (tableroInteractivo2) + '/bbService';
+    console.log("URL Calculada: " + url);
+    return url;
 }
 class WSBBChannel {
     constructor(URL, callback) {
